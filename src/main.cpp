adfe07a7496eca0a22609150fb3c21c4d809afa2
HieuHayHo <truongxuanhieu11@gmail.com>
2026-07-09 11:08:54 +0700

    fix deprecated time limit

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 
     IloCplex cplex(model);
     cplex.setOut(std::cout);
-    cplex.setParam(IloCplex::TiLim, config_data.time_limit);
+    cplex.setParam(IloCplex::Param::TimeLimit, config_data.time_limit);
 
     if (cplex.solve())
     {
