218493fe83b5c9bf5b37e093626495017e5fa38d
Hieu Hay Ho <truongxuanhieu11@gmail.com>
2026-07-21 07:23:25 +0000

    remove duplicated verification

--- src/verifier/has_hole_verifier.cpp
+++ src/verifier/has_hole_verifier.cpp
@@ -10,15 +10,5 @@ bool HasHoleVerifier::verify(ConfigData &config_data, GraphData &graph_data, std
     if (!verify_span(graph_data, solution, solution_span))
         return false;
 
-    int max_label = 0;
-    for (int v = 1; v <= graph_data.num_vertices; v++)
-        max_label = std::max(max_label, solution[v]);
-
-    if (max_label != solution_span)
-    {
-        std::cout << "! VERIFY FAILED: span = " << solution_span << ", but max label = " << max_label << "\n";
-        return false;
-    }
-
     return true;
 }
